ee8acd621481ed68b4da8c7ea7c9df231eb7f702
albertTP18 <pnthai2s9@gmail.com>
2023-10-07 23:25:52 +0700

    Add main files

---
+++ full_backtracking.py
@@ -0,0 +1,117 @@
+'''
+    Thuật toán Gradient Descent - thủ tục Backtracking
+    Bài toán hồi quy logistic
+'''
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import time
+
+# Hàm sigmoid
+def sigmoid(S):
+    return 1 / (1 + np.exp(-S))
+    # a Nx1 array
+
+# Hàm tính xác suất của x - giá trị y dự đoán
+def y_predict(w, x):
+    return sigmoid(x.dot(w))
+    # a Nx1 array
+
+# Hàm mất mát (sử dụng hệ số chính quy hóa)
+def loss(w, x, y):
+    z = y_predict(w, x)
+    return np.mean(- y * np.log(z) - (1 - y) * np.log(1 - z) + lam / 2 * np.linalg.norm(w)**2)
+    # a number
+
+# Gradient của hàm mất mát (sử dụng hệ số chính quy hóa)
+def grad_loss(w, x, y):
+    z = y_predict(w, x)
+    return np.dot(x.T, z - y) / x.shape[0] + lam * w
+    # a dx1 array
+
+# Gradient Descent (backtracking)
+def GD_bt(w, x, y, alpha, beta, epsilon):
+    i = 1  # Số vòng lặp
+    loss_hist = [loss(w, x, y)]
+    gradLoss_hist = [np.linalg.norm(grad_loss(w, x, y))]
+    while True:
+        t = 1
+        gradLoss = grad_loss(w, x, y)
+        norm_gradLoss = np.linalg.norm(gradLoss)
+        while True:
+            w_new = w - t * gradLoss
+
+            if loss(w_new, x, y) - loss(w, x, y) <= - alpha * t * norm_gradLoss ** 2:
+                break
+            else:
+                t = beta * t
+
+        loss_hist.append(loss(w_new, x, y))
+        gradLoss_hist.append(np.linalg.norm(grad_loss(w_new, x, y)))
+
+        if np.linalg.norm(grad_loss(w_new, x, y)) < epsilon:
+            print("Số vòng lặp:", i)
+            return w_new, loss_hist, gradLoss_hist
+        else:
+            w = w_new
+            i = i + 1
+            print(i, loss(w_new, x, y), np.linalg.norm(grad_loss(w_new, x, y)))
+
+# Load data từ file csv
+data = pd.read_csv('CHD_preprocessed.csv').values
+N, d = data.shape
+x = data[:, 0:d - 1]
+y = data[:, d - 1].reshape(-1, 1)
+
+# Thêm cột giá trị 1 vào dữ liệu x
+x = np.hstack((np.ones((N, 1)), x))
+
+# Train-test split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+# Bước khởi tạo cho thuật toán GD_bt
+w = np.full(x.shape[1], 0).reshape(-1, 1)
+alpha = 0.5
+beta = 0.5
+epsilon = 0.05
+lam = 0.01
+
+# Chạy thuật toán GD_bt và tính thời gian thực thi
+t1 = time.time()  # Lưu thời điểm trước khi chạy
+w, loss_hist, gradLoss_hist = GD_bt(w, x_train, y_train, alpha, beta, epsilon)
+t2 = time.time()  # Lưu thời điểm sau khi chạy
+
+print("Trọng số cần tìm:", w.T)
+print("Thời gian thực thi:", t2 - t1, "seconds")
+
+# Vẽ giá trị hàm loss
+plt.plot(loss_hist)
+plt.xlabel('Vòng lặp', fontsize=13)
+plt.ylabel('Hàm mất mát', fontsize=13)
+plt.tick_params(axis='both', which='major', labelsize=13)
+
+plt.show()
+
+# Vẽ gradient hàm loss
+plt.plot(gradLoss_hist)
+plt.xlabel('Vòng lặp', fontsize=13)
+plt.ylabel('Chuẩn của gradient hàm mất mát', fontsize=13)
+plt.tick_params(axis='both', which='major', labelsize=13)
+
+plt.show()
+
+# Tính giá trị y dự đoán của tập kiểm tra
+y_predict_test = y_predict(w, x_test)
+
+# Chuyển thành dạng nhị phân sử dụng ngưỡng 0.5
+threshold = 0.5
+y_predict_test = np.where(y_predict_test >= threshold, 1, 0)
+
+# Đánh giá accuracy
+accuracy = accuracy_score(y_test, y_predict_test)
+print("Độ chính xác", accuracy)
+
